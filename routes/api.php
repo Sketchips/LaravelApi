df51a3f2497d73778eb442e84e5fbbbd72d08cdf
Untara Eka Saputra <untara337@gmail.com>
2024-10-24 17:46:07 +0700

    AuthLogin1

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\AddProductController;
+use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +18,27 @@ use App\Http\Controllers\AddProductController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route untuk logout
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Route untuk registrasi
+Route::post('/register', [AuthController::class, 'register']);
+
+// Route untuk login
+Route::post('/login', [AuthController::class, 'login']);
+
+// Route untuk mengambil semua pengguna
+Route::get('/users', [AuthController::class, 'index']);
+
+// Route untuk mengambil data pengguna berdasarkan ID
+Route::get('/user/{id}', [AuthController::class, 'getUserData']);
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//return $request->user();
+//});
+
 
 Route::get('/stores', [StoreController::class, 'index']);
 Route::post('/stores', [StoreController::class, 'store']);
