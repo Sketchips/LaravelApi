e3f74f02246021e961bdaf680c36c3abde71cb47
Untara Eka Saputra <untara337@gmail.com>
2025-01-09 18:53:50 +0700

    gass

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\AdminResource\Widgets\UserChart;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -17,6 +18,12 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\Resources\AdminResource\Widgets\UserTypeOverview;
+use App\Filament\Resources\UserResource\Widgets\UserTypeOverview as UserTypeOverviewUser;
+use App\Filament\Pages\Dashboard;
+use App\Filament\Resources\AdminResource\Widgets\TopOrders;
+use App\Filament\Widgets\DateTimeWidget;
+use App\Filament\Resources\AdminResource\Widgets\DateTime;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -34,11 +41,16 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
+
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
+                DateTime::class,
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                UserTypeOverview::class,
+                UserChart::class,
+                TopOrders::class,
+
             ])
             ->middleware([
                 EncryptCookies::class,
